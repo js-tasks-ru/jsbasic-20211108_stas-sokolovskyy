fc38974c40d0101422df840cf84958d89a45df05
Stas Sokolovskyy <stassokolovskyy@gmail.com>
2021-11-11 12:38:17 +0200

    1st task update

--- 0-module/1-task/index.js
+++ 0-module/1-task/index.js
@@ -2,10 +2,10 @@ function sum(a, b) {
 return a + b;
 }
 
-let result = sum(1, 1);
+/* let result = sum(1, 1);
 
 let resultTwo = sum(2, 5);
 
 console.log(result);
 
-console.log(resultTwo);
+console.log(resultTwo); */
